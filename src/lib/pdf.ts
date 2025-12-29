440508a8fbadcc72f0abcafc64c1af7cc7692497
dev-aiventory <dev@aiventory.org>
2025-12-29 09:59:17 -0500

    fix

--- src/lib/pdf.ts
+++ src/lib/pdf.ts
@@ -1,3 +1,37 @@
+// Polyfill DOMMatrix for serverless environments (Vercel, etc.)
+// pdfjs-dist requires Canvas API which doesn't exist in Node.js
+if (typeof globalThis.DOMMatrix === 'undefined') {
+    // @ts-ignore - Minimal polyfill for text extraction only
+    globalThis.DOMMatrix = class DOMMatrix {
+        a = 1; b = 0; c = 0; d = 1; e = 0; f = 0;
+        m11 = 1; m12 = 0; m13 = 0; m14 = 0;
+        m21 = 0; m22 = 1; m23 = 0; m24 = 0;
+        m31 = 0; m32 = 0; m33 = 1; m34 = 0;
+        m41 = 0; m42 = 0; m43 = 0; m44 = 1;
+        is2D = true;
+        isIdentity = true;
+        constructor(init?: string | number[]) {
+            if (Array.isArray(init) && init.length >= 6) {
+                this.a = this.m11 = init[0];
+                this.b = this.m12 = init[1];
+                this.c = this.m21 = init[2];
+                this.d = this.m22 = init[3];
+                this.e = this.m41 = init[4];
+                this.f = this.m42 = init[5];
+            }
+        }
+        multiply() { return new DOMMatrix(); }
+        inverse() { return new DOMMatrix(); }
+        translate() { return new DOMMatrix(); }
+        scale() { return new DOMMatrix(); }
+        rotate() { return new DOMMatrix(); }
+        flipX() { return new DOMMatrix(); }
+        flipY() { return new DOMMatrix(); }
+        transformPoint(point: any) { return point || { x: 0, y: 0, z: 0, w: 1 }; }
+        toFloat32Array() { return new Float32Array(16); }
+        toFloat64Array() { return new Float64Array(16); }
+    };
+}
 
 import { PDFDocumentProxy } from 'pdfjs-dist';
 // @ts-ignore
